6e6b31e98559ae488545f22032c9082abaea75fc
Diogo Reis <diogokings@gmail.com>
2020-11-01 20:23:54 -0300

    Implements user update

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -3,17 +3,14 @@ import User from '../models/User';
 class UserController {
     async store(req, res) {
 
-        const userExist = await User.findOne({
-            where: {
-                email: req.body.email
-            }
-        })
+        const userExist = await findUserByEmail(req.body.email);
 
         if (userExist) {
             return res.status(400).json({
                 error: 'Usuario já existe'
             })
         }
+
         const { id, name, email } = await User.create(req.body);
         
         return res.json({
@@ -24,8 +21,47 @@ class UserController {
     }
 
     async update(req, res) {
-        return res.json({ ok: true });
+        const { email, oldPassword } = req.body;
+
+        const user = await User.findByPk(req.userId);
+
+        console.log(user.email);
+
+        if (email !== user.email) {
+            
+            const userExist = await findUserByEmail(email);
+
+            console.log(userExist);
+
+            if (userExist) {
+                return res.status(400).json({
+                    error: 'Esse email já existe'
+                });
+            }
+        }
+
+        if (oldPassword && !(await user.checkPassword(oldPassword))) {
+            return res.status(401).json({
+                error: 'Senha incorreta'
+            });
+        }
+        
+        const { id, name } = await user.update(req.body);
+
+        return res.json({
+            id,
+            name,
+            email
+        });
     }
+}
+
+function findUserByEmail(email) {
+    return User.findOne({
+        where: {
+            email: email
+        }
+    })
 }
 
 export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -12,6 +12,6 @@ routes.post('/users', UserController.store);
 
 routes.use(authMiddleware);
 
-routes.put('/users', UserController.update)
+routes.put('/users', UserController.update);
 
 export default routes;
